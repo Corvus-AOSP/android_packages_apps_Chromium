db8b895b3dc5e6e1feb73855ac5908366d00f221
Chirayu Desai <chirayudesai1@gmail.com>
2020-11-14 05:24:31 +0530

    Add Android.mk
    From: CalyxOS/platform_prebuilts_calyx
    Commit: 0ad9f89ea1a539d3218e23237be86119c7b68928
    Message: Chromium: Update Android.mk for Trichrome
    
    Only change I did was dropping my_src_arch, and updating path in LOCAL_SRC_FILES
    
    Change-Id: I041ea21f23fec040651ad0f54654ef046a8675c5

---
+++ Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := TrichromeLibrary
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_SRC_FILES := TrichromeLibrary.apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := TrichromeWebView
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MULTILIB := both
+LOCAL_REQUIRED_MODULES := \
+        libwebviewchromium_loader \
+        libwebviewchromium_plat_support \
+        TrichromeLibrary
+LOCAL_OVERRIDES_PACKAGES := webview
+LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_SRC_FILES := TrichromeWebView.apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := TrichromeChrome
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MULTILIB := both
+LOCAL_REQUIRED_MODULES := \
+        TrichromeLibrary
+LOCAL_OVERRIDES_PACKAGES := Browser2
+LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_SRC_FILES := TrichromeChrome.apk
+include $(BUILD_PREBUILT)
